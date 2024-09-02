2e3624dfe93f72a51db817504aa0090cec763ee6
alx-2023 <u202218649@upc.edu.pe>
2024-09-01 20:54:30 -0500

    Modificación de nombre de repositorio

--- src/main/java/pe/edu/upc/dermacheck/controllers/CentroMedicosController.java
+++ src/main/java/pe/edu/upc/dermacheck/controllers/CentroMedicosController.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/centrosmedicos")
-public class CentroMedicosControler {
+public class CentroMedicosController {
     @Autowired
     private ICentroMedicosService cS;
 
