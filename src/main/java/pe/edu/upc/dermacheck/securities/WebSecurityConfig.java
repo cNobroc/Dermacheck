55e8c51fce3d3dffebb1f6f76fcee00097f8cf11
cNobroc <samuelpaucar718@gmail.com>
2024-09-20 16:51:00 -0500

    Implementacion de security en Swagger

--- src/main/java/pe/edu/upc/dermacheck/securities/WebSecurityConfig.java
+++ src/main/java/pe/edu/upc/dermacheck/securities/WebSecurityConfig.java
@@ -59,6 +59,7 @@ public class WebSecurityConfig {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
+                        .requestMatchers(AUTH_WHITELIST).permitAll()
                         .requestMatchers(antMatcher("/login")).permitAll()
                         .anyRequest().authenticated()
                 )
@@ -69,5 +70,14 @@ public class WebSecurityConfig {
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
+
+    private static final String[] AUTH_WHITELIST = {
+
+            "/api/v1/auth/**",
+            "/v3/api-docs/**",
+            "/v3/api-docs.yaml",
+            "/swagger-ui/**",
+            "/swagger-ui.html"
+    };
 
 }
